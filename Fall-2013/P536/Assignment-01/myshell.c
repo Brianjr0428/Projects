b3b6c44c6bf5157f32044f874bd34595092f2d5f
Tzu-Feng Wu <tzwu@indiana.edu>
2013-09-04 09:32:46 -0400

    Update myshell.c

--- Fall-2013/P536/Assignment-01/myshell.c
+++ Fall-2013/P536/Assignment-01/myshell.c
@@ -5,6 +5,7 @@
 #define MAX_LENGTH 1024
 
 int splitWords(char*, char**, int);
+void execute(char**);
 
 int main(){
 	char line[MAX_LENGTH];
@@ -14,17 +15,10 @@ int main(){
 		char  *argv[MAX_LENGTH];
 		//split line into word array;
 		int count=splitWords(line, argv, 0);
-		
-		//test the word array, should be delated later;
-		int i;
-		for(i=0;i<count;i++){
-			if(i==count-1)
-				printf("%s",argv[i]);
-			else
-				printf("%s\n",argv[i]);
-		}
-			
-		//system(line);
+		if(strcmp(argv[0],"exit") == 0)
+			exit(1);
+		else
+			execute(argv); //execute argv
 	}
 	
 	return 0;
@@ -32,10 +26,27 @@ int main(){
 
 int splitWords(char* line, char** argv, int count){
 	char* tok;
-	tok=strtok(line, " ");
+	tok=strtok(line, " \n	");
 	while(tok!= NULL){
 		argv[count++]=tok;//malloc((strlen(tok)+1));
 		tok=strtok(NULL, " ");		
 	}
 	return count;
+}
+
+void execute(char **argv){
+	pid_t pid=fork();
+	int status;
+	if(pid<0){
+		perror("error");
+		exit(1);
+	}
+		
+	else if(pid==0){
+		execvp(*argv, argv);
+	}
+	else{
+		waitpid(-1, &status, 0);
+	}
+
 }
