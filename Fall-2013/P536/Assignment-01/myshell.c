9696793a36ae3fb9ea5cbad97f4d6348d6e69b1e
Tzu-Feng Wu <tzwu@indiana.edu>
2013-09-11 21:02:45 -0400

    Update myshell.c

--- Fall-2013/P536/Assignment-01/myshell.c
+++ Fall-2013/P536/Assignment-01/myshell.c
@@ -10,8 +10,9 @@
 #define MAX_LENGTH 1024
 
 void  parse(char*, char**);
-void executeOneCommand(char*);
-void execute(char**, int , bool);
+int initParse(char*, bool*, char**);
+void executeOneCommand(char*, bool);
+void execute(char*, bool);
 
 int i;
 bool back;
@@ -19,112 +20,142 @@ int pidf=0;
 int pidb=0;
 
 int main(){
+	
 	char line[MAX_LENGTH];
 	while(1){
+		back=false;
 		signal (SIGCHLD, sig_fork);
 		printf("myshell: ");
 		if(fgets(line, MAX_LENGTH, stdin)==NULL)
 			exit(1);
-
 		if(!strcmp(line,"\n"))
 			continue;
 		line[strlen(line)-1]='\0';
+		if(!checkSyntax(line)){
+			if(pidb<pidf){
+				printf("[%d]+	Done\n",++pidb);
+				pidf=0;
+				pidb=0;
+			}
+			exit(1);	
+		}
 		if(!strcmp(line,"exit"))
 			exit(1);
-		char *dirLine;
+		bool stats[MAX_LENGTH];
+		for(i=0;i<MAX_LENGTH;i++)
+			stats[i]=false;
+		char *commands[MAX_LENGTH];
+		int cmd=initParse(line,stats,commands);
+		
+		for(i=0;i<cmd;i++)
+			executeOneCommand(commands[i], stats[i]);	
+	}	
+	return 0;
+}
+
+void executeOneCommand(char *line, bool stat){
+		while(*line==' ')
+			*line++;
 		if(isCd(line)){
+			char *dirLine;
 			dirLine=trimSpace(line,dirLine);
+			if(strlen(dirLine)==0)
+				dirLine=getenv("HOME");
 			if(chdir(dirLine)<0){
 				char msg[MAX_LENGTH];
 				sprintf(msg, "-bash: cd: %s", dirLine);
 				perror(msg);
 			}
-			continue;
-		}
-		int cmd=0;
-		char *commands[MAX_LENGTH];
-		char* tok;
-		tok = strtok(line, ";");		
-		while(tok != NULL){
-			commands[cmd++]=tok;
-			tok=strtok(NULL, ";");
-		}
-		for(i=0;i<cmd;i++)
-			executeOneCommand(commands[i]);
-	}	
-	return 0;
-}
-
-void executeOneCommand(char *line){
-		back=false;
-		char *argv[MAX_LENGTH];
-		//split line into word array;
-		parse(line, argv);
-		int index=0;
-		int len=0;
-		int i;
-		while(argv[index++]!='\0')
-			len++;
-		int idNum[MAX_LENGTH];
-		pid_t pid=fork();
-		int status;
-		if(pid<0){
-			perror("error");
-			exit(1);
-		}
-		else if(pid==0){
-			int checkOut=outCheck(argv, len);
-			if(checkOut==1||checkOut==2){
-				int fd;
-				if(checkOut==1)
-					fd= open(argv[len-1], O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR) ;
-				else if(checkOut==2)
-					fd= open(argv[len-1], O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR) ;
-				if(fd == -1){
-					perror("open");
-					return exit(1);
-				}
-				dup2(fd, STDOUT_FILENO);
-				dup2(fd, STDERR_FILENO);	
-				close(fd);
-				char  *argv2[MAX_LENGTH];
-				
-				for(i=0;i<len-2;i++)
-					argv2[i]=argv[i];
-				execute(argv2,len-2,pipeCheck(argv2,len-2)); //execute argv
-			}
-			else
-				execute(argv, len, pipeCheck(argv,len)); //execute argv
-		}
-		else{
-			if(back){
-				idNum[pidf]=(int)pid;
-				printf("[%d] %d\n",pidf+1, idNum[pidf]);
-				pidf++;
-				if(pidf>1){
-					printf("[%d]	Done\n",++pidb);
-				}
-					
-			}
-			else{
-				while(wait(&status)>0);
-				if(pidb<pidf){
+			if(!back&&pidb<pidf){
 					printf("[%d]+	Done\n",++pidb);
 					pidf=0;
 					pidb=0;
 				}	
-				usleep(100000);
-			}
-		}	
-}
-
-void execute(char **argv, int len, bool hasPipe){
-	if(!hasPipe){
-		if(inputCheck(argv,len)){
-			inputReplace(argv,len);
-			execute(argv, len+1, pipeCheck(argv,len+1));
 		}
 		else{
+			back=stat;
+			//split line into word array;
+			int idNum[MAX_LENGTH];
+			pid_t pid=fork();
+			int status;
+			if(pid<0){
+				perror("error");
+				exit(1);
+			}
+			else if(pid==0){
+				int checkOut=outCheck(line);
+				if(checkOut==1||checkOut==2){
+					char *lines[MAX_LENGTH];
+					parseOutput(line,lines);
+					lines[1]=trimWord(lines[1]);
+					int fd;
+					if(checkOut==1){
+						fd= open(lines[1], O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR) ;
+					}else if(checkOut==2){
+						fd= open(lines[1], O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);	
+					}
+										
+					if(fd == -1){
+						perror("open");
+						return exit(1);
+					}
+					dup2(fd, STDOUT_FILENO);
+					dup2(fd, STDERR_FILENO);	
+					close(fd);
+					execute(lines[0],pipeCheck(lines[1])); //execute argv
+				}
+				else
+					execute(line,pipeCheck(line)); //execute argv
+			}
+			else{
+				if(back){
+					idNum[pidf]=(int)pid;
+					printf("[%d] %d\n",pidf+1, idNum[pidf]);
+					pidf++;
+					if(pidf>1){
+						printf("[%d]	Done\n",++pidb);
+					}
+				}
+				else{
+					while(wait(&status)>0);
+					if(pidb<pidf){
+						printf("[%d]+	Done\n",++pidb);
+						pidf=0;
+						pidb=0;
+					}	
+					usleep(100000);
+				}
+			}	
+		}
+
+}
+
+void execute(char *line, bool hasPipe){
+	
+	if(!hasPipe){
+		char  *argv[MAX_LENGTH];
+		if(inputCheck(line)){
+			char *lines[MAX_LENGTH];
+			parseInput(line, lines);
+			lines[1]=trimWord(lines[1]);
+			int fd ;
+			if((fd= open(lines[1], O_RDONLY, S_IRUSR)) == -1){
+				char msg[MAX_LENGTH];
+				sprintf(msg, "-bash: %s", lines[1]);
+				perror(msg);
+				return exit(1);
+			}
+			dup2(fd, STDIN_FILENO);
+			dup2(fd, STDERR_FILENO);	
+			close(fd);
+			parse(lines[0], argv);
+			if(execvp(*argv, argv)<0){
+				printf("-bash: %s: command not found\n", argv[0]);
+				exit(1);
+			}
+		}
+		else{
+			parse(line, argv);
 			if(execvp(*argv, argv)<0){
 				printf("-bash: %s: command not found\n", argv[0]);
 				exit(1);
@@ -132,23 +163,8 @@ void execute(char **argv, int len, bool hasPipe){
 		}
 	}		
 	else{
-		char *front[MAX_LENGTH];
-		char *end[MAX_LENGTH];
-		int i;
-		int index=0;
-		int index1=0;
-		int index2=0;
-		bool meetPipe=false;
-		int pIndex=findLastPipe(argv,len);
-		while(index<len){
-			if(index<pIndex){
-				front[index1++]=argv[index];
-			}
-			else if (index > pIndex){
-				end[index2++]=argv[index];
-			}
-			index++;
-		}
+		char *lines[MAX_LENGTH];
+		parsePipe(line, lines);
 		int fd[2];
 		pipe(fd);
 		int pid=fork();
@@ -158,36 +174,47 @@ void execute(char **argv, int len, bool hasPipe){
 			exit(1);
 		}
 		else if(pid==0){
+			char *exlines[MAX_LENGTH];
+			parse(lines[1], exlines);
 			dup2(fd[0],0);
 			close(fd[1]);
-			if(execvp(*end, end)<0){
-				printf("-bash: %s: command not found\n", end[0]);
+			if(execvp(*exlines, exlines)<0){
+				printf("-bash: %s: command not found\n", exlines[0]);
 				exit(1);
 			}
 		}
 		else{
 			dup2(fd[1],1);
 			close(fd[0]);
-			execute(front, index1, pipeCheck(front,index1));			
+			execute(lines[0], pipeCheck(lines[0]));			
 			while(wait(&status)>0);
 		}
 	}
+
 }
 
-void  parse(char *line, char **argv)
+int initParse(char *line, bool *stats, char **argv)
 {
+	int count=0;
+	bool tempStat=false;
 	while (*line != '\0') {
-		while (*line == ' ' || *line == '\t' || *line == '\'' || *line == '\"'|| *line == '&'|| *line == '\n'){
+		while (*line == '&' || *line == ';' || *line == '\n'){
 			if(*line == '&')
-				back=true;
+				tempStat=true;
+			else 
+				tempStat=false;
+			if(count>0)	
+					stats[count-1]=tempStat;
 			*line++ = '\0';
 		}
 		if(*line == '\0')
 			break;
-		if(!back){
-			*argv++= line;     
-			while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n'&& *line != '\''&& *line != '\"') 
-				line++;             		}
+		*argv++= line;  
+		
+		count++;  
+		while (*line != '\0' && *line != ';'&& *line != '&'&& *line != '\t' && *line != '\n') 
+			line++;
 	}
-     *argv = '\0';                
+     *argv = '\0';    
+	return count;            
 }
